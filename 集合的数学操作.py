a09f9396a47cfa7d5d4440d36eca19b57577fb69
张荣超 <372798663@qq.com>
2018-03-06 23:16:10 +0800

    Create 集合的数学操作.py

---
+++ 集合的数学操作.py
@@ -0,0 +1,90 @@
+
+"""集合的数学操作"""
+
+"""
+两个集合的常见数学操作如下图所示：
+
+"""
+
+"""
+一、两个集合的交集
+"""
+s1 = {1, 3, 5, 7, 9}
+s2 = {2, 3, 6, 7, 10}
+"""
+    调用方法intersection和使用运算符&是等价的。
+    做交集操作后生成一个新集合，做交集操作的两个集合不变。
+"""
+print(s1.intersection(s2))  # {3, 7}
+print(s1 & s2)              # {3, 7}
+print(s1)                   # {1, 3, 5, 7, 9}
+print(s2)                   # {2, 3, 6, 7, 10}
+"""
+    s1.intersection_update(s2)的执行结果：
+        用s1.intersection(s2)的返回值更新s1，s2不变。
+        方法intersection_update的返回值为None。
+"""
+print(s1.intersection_update(s2))   # None
+print(s1)                   # {3, 7}
+print(s2)                   # {2, 3, 6, 7, 10}
+
+"""
+二、两个集合的并集
+"""
+s1 = {1, 3, 5, 7, 9}
+s2 = {2, 3, 6, 7, 10}
+"""
+    调用方法intersection和使用运算符&是等价的。
+    做并集操作后生成一个新集合，做并集操作的两个集合不变。
+"""
+print(s1.union(s2))         # {1, 2, 3, 5, 6, 7, 9, 10}
+print(s1 | s2)              # {1, 2, 3, 5, 6, 7, 9, 10}
+print(s1)                   # {1, 3, 5, 7, 9}
+print(s2)                   # {2, 3, 6, 7, 10}
+"""
+    注意：不存在方法union_update()。
+"""
+
+"""
+三、两个集合的差集
+"""
+s1 = {1, 3, 5, 7, 9}
+s2 = {2, 3, 6, 7, 10}
+"""
+    调用方法difference和使用运算符-是等价的。
+    做差集操作后生成一个新集合，做差集操作的两个集合不变。
+"""
+print(s1.difference(s2))    # {1, 5, 9}
+print(s1 - s2)              # {1, 5, 9}
+print(s1)                   # {1, 3, 5, 7, 9}
+print(s2)                   # {2, 3, 6, 7, 10}
+"""
+    s1.difference_update(s2)的执行结果：
+        用s1.difference(s2)的返回值更新s1，s2不变。
+        方法difference_update的返回值为None。
+"""
+print(s1.difference_update(s2))   # None
+print(s1)                   # {1, 5, 9}
+print(s2)                   # {2, 3, 6, 7, 10}
+
+"""
+四、两个集合的对称差集
+"""
+s1 = {1, 3, 5, 7, 9}
+s2 = {2, 3, 6, 7, 10}
+"""
+    调用方法symmetric_difference和使用运算符^是等价的。
+    做对称差集操作后生成一个新集合，做对称差集操作的两个集合不变。
+"""
+print(s1.symmetric_difference(s2))      # {1, 2, 5, 6, 9, 10}
+print(s1 ^ s2)                          # {1, 2, 5, 6, 9, 10}
+print(s1)                               # {1, 3, 5, 7, 9}
+print(s2)                               # {2, 3, 6, 7, 10}
+"""
+    s1.symmetric_difference_update(s2)的执行结果：
+        用s1.symmetric_difference(s2)的返回值更新s1，s2不变。
+        方法symmetric_difference_update的返回值为None。
+"""
+print(s1.symmetric_difference_update(s2))   # None
+print(s1)                   # {1, 2, 5, 6, 9, 10}
+print(s2)                   # {2, 3, 6, 7, 10}
