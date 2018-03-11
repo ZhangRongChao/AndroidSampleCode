be6d53b9b3c3e08169e71470bd72e96703b46e07
张荣超 <372798663@qq.com>
2018-03-11 15:18:08 +0800

    Create 在文本编辑器中编写HelloWorld程序.py

---
+++ 在文本编辑器中编写HelloWorld程序.py
@@ -0,0 +1,32 @@
+
+"""在文本编辑器中编写HelloWorld程序"""
+
+"""
+一、选择一款比较流行的文本编辑器
+    Windows操作系统上比较流行的文本编辑器有：Notepad++、EditPlus、UltraEdit等。 
+    MacOS操作系统上比较流行的文本编辑器有：Sublime、TextMate、BBEdit等。
+    Ubuntu操作系统上比较流行的文本编辑器有：Sublime、UltraEdit等。
+
+二、在文本编辑器中编写HelloWorld程序并保存为py文件
+    在文本编辑器中编写如下HelloWorld程序:
+    print('hello, world') # 打印字符串'hello, world'
+    然后选择一个保存路径，例如：下载文件夹，
+    将其保存为文件名为hw且后缀名为py的文件。
+
+三、执行py文件
+    在Windows操作系统中，打开命令提示符窗口；
+    在MacOS或Ubuntu操作系统中，打开终端窗口，
+    切换到hw.py文件所在的目录，例如：cd Downloads，
+    输入命令：python hw.py（Windows）或：python3 hw.py（MacOS或Ubuntu），然后按回车键。
+    这样，解释器就会执行hw.py文件，从而打印出字符串'hello, world'。
+    
+四、py文件并不会自动打印表达式的值
+    在REPL交互式命令行中，解释器会自动打印表达式的值，例如：
+    >>> 'hello, world'
+    'hello, world'
+
+    在py文件中，解释器不会自动打印表达式的值，例如：
+    将hw.py文件中的print语句改为：'hello, world'，
+    然后执行hw.py文件，并不会打印出字符串'hello, world'。
+"""
+
