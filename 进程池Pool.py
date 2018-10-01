d02f89a92c5fb38757775993a91daa41151cbe0e
张荣超 <372798663@qq.com>
2018-10-01 20:11:41 +0800

    Update 进程池Pool.py

--- 进程池Pool.py
+++ 进程池Pool.py
@@ -1,45 +1,68 @@
 
-"""进程池Pool"""
+"""进程池ProcessPoolExecutor（3）"""
 
 """
-    如果并发的任务数过多，一次性创建与启动大量的进程会给计算机带来很大的压力，那么就可以使用进程池
-对创建与启动的进程进行限制和管理。
-    进程池中所能容纳的进程数目是固定的。当需要创建与启动一个子进程时，如果进程池还没满，则会
-创建与启动一个子进程并添加到进程池中；如果进程池已经满了，则会一直等待，直到进程池中有任意一个
-子进程结束从而腾出一个位置，才会创建与启动一个子进程并添加到进程池中。
-    标准库模块multiprocessing中提供了一个类对象Pool，用于表示进程池。进程池中所能容纳的进程数目
-可以在创建Pool实例对象时进行指定；如果不指定，默认大小是CPU的核数。
+    标准库模块concurrent.futures中还提供了两个函数：
+（1）wait(fs, timeout=None, return_when=ALL_COMPLETED)
+    该函数用于阻塞主进程，以等待指定的Future实例对象序列，直到满足指定的条件。
+    参数fs用于指定要等待的Future实例对象序列。
+    参数timeout用于指定等待的最长时间。如果指定为None或不指定，则一直等待。
+    参数return_when用于指定该函数何时返回，有三种取值：FIRST_COMPLETED、FIRST_EXCEPTION
+和ALL_COMPLETED，分别表示：当第一个Future实例对象已经完成或已被取消时、当第一个Future实例对象
+抛出异常时、当所有Future实例对象已经完成或已被取消时。
+    该函数的返回值是由两个集合组成的元组，第一个集合包含了已经完成或已被取消的所有Future实例对象，
+第二个集合包含了没有完成并且没有被取消的所有Future实例对象。
+（2）as_completed(fs, timeout=None)
+    该函数用于将指定的Future实例对象序列转换为一个迭代器，当序列中的任意一个Future实例对象
+已经完成或已被取消时都会被yield。这样，通过遍历得到的迭代器，我们就可以在任意一个Future实例对象
+已经完成或已被取消时立即做一些处理，比如调用方法result()得到执行结果。
+    参数fs用于指定Future实例对象序列。
+    参数timeout用于指定超时时间。如果指定为None或不指定，则不会超时。
+    该函数的返回值是Future实例对象的迭代器。
 """
-from multiprocessing import Pool
+from concurrent.futures import ProcessPoolExecutor, wait, ALL_COMPLETED, \
+    FIRST_COMPLETED, as_completed
 import time, random
 
-print('父进程启动')
-
 def do_sth(i):
-    print('子进程%d启动' % i)
-
-    start = time.time()
     time.sleep(random.random() * 10)
-    end = time.time()
+    return i * i
 
-    print('子进程%d结束，耗时%.2f秒' % (i, end - start))
+ppe = ProcessPoolExecutor(max_workers=3)
 
-# 将进程池中所能容纳的最大进程数指定为3
-pp = Pool(3)
+"""
+objs = []
+for i in range(1, 5):
+    future = ppe.submit(do_sth, i)
+    objs.append(future)
 
-for i in range(1, 11):
-    # 与方法start()类似，不同的是，创建并启动由进程池管理的子进程
-    pp.apply_async(do_sth, args=(i,))   # 异步
-    # pp.apply(do_sth, args=(i,))       # 同步
+#(done, not_done) = wait(objs, return_when=ALL_COMPLETED)
+(done, not_done) = wait(objs, return_when=FIRST_COMPLETED)
 
-# 调用方法join()之前必须先调用方法close()
-# 调用方法close()之后就不能让进程池再管理新的进程了
-pp.close()
+print(done)
+print(not_done)
+"""
 
-# 父进程被阻塞
-# 进程池管理的所有子进程执行完之后，父进程再从被阻塞的地方继续执行
-pp.join()
+"""
+objs = []
+for i in range(1, 5):
+    future = ppe.submit(do_sth, i)
+    objs.append(future)
 
-print('父进程结束')
+future_iterator = as_completed(objs)
+for future in future_iterator:
+    print(future.result())
+"""
 
-# 程序运行后会同时创建并启动3个子进程，第4个子进程要等前面3个中的某一个执行结束后才会创建并启动
+"""
+    Future提供了一个方法add_done_callback(self, fn)，用于指定Future实例对象已经完成或
+已被取消时被立即回调的函数fn，该Future实例对象将作为fn的唯一实参。
+    方法add_done_callback()与函数as_completed()都可以实现在Future实例对象已经完成或
+已被取消时立即做一些处理的需求。
+"""
+def get_result(future):
+    print(future.result())
+
+for i in range(1, 5):
+    future = ppe.submit(do_sth, i)
+    future.add_done_callback(get_result)
