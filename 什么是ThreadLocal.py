82734d2584c04daabd43a8995ffc38ecb26f9a78
张荣超 <372798663@qq.com>
2018-10-03 20:17:35 +0800

    Update 什么是ThreadLocal.py

--- 什么是ThreadLocal.py
+++ 什么是ThreadLocal.py
@@ -1,51 +1,113 @@
 
-"""进程间通信之管道"""
+"""多线程同步之Condition"""
 
 """
-    如果想要实现进程之间的通信，管道是常见的实现方式之一。
+    标准库模块threading中提供了一个类对象Condition，用于表示带触发条件的锁，以帮助我们处理
+多线程间复杂的同步问题。Condition允许一个或多个线程等待触发条件，直到收到另外一个线程的通知。
+    Condition除了具有Lock和RLock的方法acquire()和release()外，还有以下几个方法：
+（1）__init__(self, lock=None)
+    参数lock用于指定一个Lock或RLock实例对象作为底层的锁。
+    默认值None表示会自动创建一个RLock实例对象作为底层的锁。
+    调用Condition实例对象的方法acquire()和release()，其实调用的是底层的锁的相应方法。
+（2）wait(self, timeout=None)
+    该方法用于等待触发条件，直到收到另外一个线程的通知，或直到超过了参数timeout指定的时间。
+其中，参数timeout必须是一个浮点数，单位是秒。
+    调用该方法的线程必须已经获得了锁，否则会抛出异常RuntimeError。
+    调用该方法后，线程会释放锁并被阻塞，还会被放到条件等待池中。
+    如果超过了参数timeout指定的时间还没有收到另外一个线程的通知，该方法返回False；
+当收到了另外一个线程的通知时，如果没有指定timeout或在timeout指定的时间内，该方法返回True。
+    当收到了另外一个线程的通知，或超过了参数timeout指定的时间后，都会自动调用方法acquire()
+试图获得锁，获得锁失败的线程会从条件等待池转移到锁等待池。当另外一个线程调用方法release()释放锁之后，
+线程调度程序再从锁等待池中选择一个线程来获得锁。
+（3）wait_for(self, predicate, timeout=None)
+    该方法比上面的方法多了一个参数predicate，用于指定一个返回值为布尔值的函数，只要该函数的
+返回值不为True就一直等待。因此，如果忽略掉timeout，调用该方法就相当于：
+while not predicate():
+    con.wait()
+    调用该方法的线程必须已经获得了锁，否则会抛出异常RuntimeError。
+    调用该方法后，线程会释放锁并被阻塞，还会被放到条件等待池中。
+    如果超过了参数timeout指定的时间还没有收到另外一个线程的通知并且参数predicate指定的函数的
+返回值还不为True，该方法返回False；否则，该方法返回参数predicate指定的函数的最后一次返回值。
+    当收到了另外一个线程的通知，或超过了参数timeout指定的时间后，或参数predicate指定的函数的
+返回值为True时，都会自动调用方法acquire()试图获得锁，获得锁失败的线程会从条件等待池转移到锁等待池。
+当另外一个线程调用方法release()释放锁之后，线程调度程序再从锁等待池中选择一个线程来获得锁。
+（4）notify(n=1)
+    该方法用于通知条件等待池中等待触发条件的最多n个线程。
+    被通知的所有线程都会自动调用方法acquire()试图获得锁，获得锁失败的线程会从条件等待池转移到锁等待池。
+当另外一个线程调用方法release()释放锁之后，线程调度程序再从锁等待池中选择一个线程来获得锁。    
+    调用该方法的线程必须已经获得了锁，否则会抛出异常RuntimeError。
+    线程调用该方法后不会释放锁。
+（5）notify_all()
+    该方法用于通知条件等待池中等待触发条件的所有线程。
+    被通知的所有线程都会自动调用方法acquire()试图获得锁，获得锁失败的线程会从条件等待池转移到锁等待池。
+当另外一个线程调用方法release()释放锁之后，线程调度程序再从锁等待池中选择一个线程来获得锁。
+    调用该方法的线程必须已经获得了锁，否则会抛出异常RuntimeError。
+    线程调用该方法后不会释放锁。
+    
+    Condition也遵守了上下文管理协议，所以可以使用with语句。
 """
-from multiprocessing import Process, Pipe
-import os, time, random
+from threading import Thread, Condition
+import time
 
-# 发送数据的子进程执行的代码
-def send_data(conn):
-    print('发送数据的子进程%d启动' % os.getpid())
+cond = Condition()
 
-    for obj in [i for i in range(1, 10)]:
-        print('发送数据：%s' % obj)
-        conn.send(obj)
-        time.sleep(random.random() * 3)
+class MyThread1(Thread):
+    def __init__(self, name):
+        super().__init__(name=name)
 
-    print('发送数据：None')
-    conn.send(None)
+    def run(self):
+        # cond.acquire()
 
-    print('发送数据的子进程%d结束' % os.getpid())
+        with cond:
+            print('%s说：1' % self.name)
+            cond.notify()
+            cond.wait()
 
-# 接收数据的子进程执行的代码
-def recv_data(conn):
-    print('接收数据的子进程%d启动' % os.getpid())
+            # 思考两秒之后再说
+            time.sleep(2)
+            print('%s说：11' % self.name)
+            cond.notify()
+            cond.wait()
 
-    while True:
-        item = conn.recv()
-        if item is None:
-            print('接收数据：None')
-            break
-        print('接收数据：%s' % item)
-        time.sleep(random.random() * 3)
+            time.sleep(2)
+            print('%s说：111' % self.name)
+            cond.notify()
 
-    print('接收数据的子进程%d结束' % os.getpid())
+        # cond.release()
 
-print('父进程%d启动' % os.getpid())
+class MyThread2(Thread):
+    def __init__(self, name):
+        super().__init__(name=name)
 
-cr, cs = Pipe(False)
+    def run(self):
+        time.sleep(1)
+        # cond.acquire()
 
-ps = Process(target=send_data, args=(cs,))
-pr = Process(target=recv_data, args=(cr,))
+        with cond:
+            # 思考两秒之后再说
+            time.sleep(2)
+            print('%s说：2' % self.name)
+            cond.notify()
+            cond.wait()
 
-ps.start()
-pr.start()
+            time.sleep(2)
+            print('%s说：22' % self.name)
+            cond.notify()
+            cond.wait()
 
-ps.join()
-pr.join()
+            time.sleep(2)
+            print('%s说：222' % self.name)
 
-print('父进程%d结束' % os.getpid())
+        # cond.release()
+
+MyThread1('Thread1').start()
+MyThread2('Thread2').start()
+
+"""
+Thread1说：1
+Thread2说：2
+Thread1说：11
+Thread2说：22
+Thread1说：111
+Thread2说：222
+"""
