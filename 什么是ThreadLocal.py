9b46e52490f170a469220e3b5b88ab1b383e4104
张荣超 <372798663@qq.com>
2018-10-03 16:44:35 +0800

    Update 什么是ThreadLocal.py

--- 什么是ThreadLocal.py
+++ 什么是ThreadLocal.py
@@ -1,72 +1,30 @@
 
-"""全局解释器锁GIL"""
+"""进程间通信之Manager"""
 
 """
-    如果想要查看CPU的核心数，可以调用标准库模块multiprocessing中的函数cpu_count。
+    如果想要实现进程之间的通信，Manager也是常见的实现方式之一。
+    与共享内存相比，Manager更加灵活，因为它可以支持多种对象类型，包括：list、dict、Value、
+Array、Namespace, Lock、RLock、Semaphore、BoundedSemaphore、Condition、Event、
+Barrier、Queue等。此外，Manager还可以通过网络被不同计算机上的进程所共享。但是，Manager的速度
+比共享内存要慢。
 """
-import multiprocessing
-print(multiprocessing.cpu_count())  # 8
+from multiprocessing import Process, Manager
 
-"""
-    先来做几个实验：
-    
-一、单进程或单线程
-def do_sth():
-    while True:
-        pass
+def f():
+    d[1] = 18
+    d['2'] = 56
+    l.reverse()
 
-do_sth()
-    打开活动监视器（macOS）或任务管理器（Windows），查看CPU占用率：100%。
-这说明：单进程或单线程占满了八核CPU中的其中一核。
+manager = Manager()
 
-二、多进程
-from multiprocessing import Process
+# 通过Manager创建一个用于进程间通信的字典
+d = manager.dict()
+# 通过Manager创建一个用于进程间通信的列表
+l = manager.list(range(5))
 
-def do_sth():
-    while True:
-        pass
+p = Process(target=f)
+p.start()
+p.join()
 
-Process(target=do_sth).start()
-Process(target=do_sth).start()
-
-do_sth()
-    打开活动监视器（macOS）或任务管理器（Windows），查看CPU占用率：300%。
-这说明：三个进程占满了八核CPU中的其中三核，因此，多进程可以实现并行（同时处理多个任务）从而发挥
-多核CPU的最大功效。
-
-三、多线程
-from threading import Thread
-
-def do_sth():
-    while True:
-        pass
-
-Thread(target=do_sth).start()
-Thread(target=do_sth).start()
-
-do_sth()
-    打开活动监视器（macOS）或任务管理器（Windows），查看CPU占用率：100%。
-这说明：三个线程并没有占满八核CPU中的其中三核，而只占满了其中一核，因此，多线程并不能实现并行
-（同时处理多个任务）而只能实现并发（交替处理多个任务）。
-"""
-
-"""
-    我们编写的Python代码是通过Python解释器来执行的。通常使用的Python解释器是官方提供的CPython。
-CPython中有一个GIL（Global Interpreter Lock，全局解释器锁），其作用相当于Lock，任何线程
-在执行前必须先获得GIL，一个线程在获得GIL后其它线程就不能执行，直到该线程释放GIL。因此，GIL保证了
-同一时刻只有一个线程可以执行，从而导致Python中的多线程不能实现并行。
-    如果将CPython更换为其它的Python解释器，例如：JPython、IronPython、PyPy等，就不会存在
-GIL的问题。
-"""
-
-"""
-    对于IO密集型的任务，例如：网络请求、读写磁盘等，因为IO的速度远远低于CPU和内存的速度，所以，
-绝大部分时间都在等待IO操作的完成，CPU的消耗很少。因此，IO密集型的任务在使用多线程时，GIL无法
-利用多核的劣势体现得非常微弱。
-    一个线程在进行IO操作之前，其获得的GIL总是会被释放，以允许其它线程在该线程等待IO操作的时候
-获得GIL。
-    对于计算密集型的任务，例如：科学计算、视频处理等，主要消耗的是CPU的资源，全靠CPU的运算能力。
-因此，计算密集型的任务在使用多线程时，GIL无法利用多核的劣势体现得非常明显，此外，任务数越多，
-花在任务切换的时间就越多，CPU执行任务的效率就越低。
-    在没有IO操作时，GIL会在每隔100次操作后被释放（这个次数可以通过sys.setcheckinterval来调整）。
-"""
+print(d)
+print(l)
